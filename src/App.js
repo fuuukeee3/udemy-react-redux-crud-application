6ecbc24918a61d7b5fe9cb4e80e5b946d0bbdca4
fuuukeee3 <semarusika@gmail.com>
2020-04-16 09:07:54 +0900

    functional components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
 
-function App() {
+// function App() {
+//   return (
+//     <div>
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         onChange={() => {
+//           console.log("Hi!");
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+const App = () => {
   return (
     <div>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log("Hi!");
-        }}
-      />
+      <Cat />
+      <Cat />
     </div>
   );
-}
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
