7beb5814c8b74dfa1b13efbe4198a90df8efca9e
fuuukeee3 <semarusika@gmail.com>
2020-04-25 08:11:09 +0900

    implement eventsShow

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -12,6 +12,11 @@ class EventsShow extends Component {
     this.onDeleteClick = this.onDeleteClick.bind(this);
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    if (id) this.props.getEvent(id);
+  }
+
   renderField(field) {
     const {
       input,
@@ -28,7 +33,7 @@ class EventsShow extends Component {
   }
 
   async onSubmit(values) {
-    // await this.props.postEvent(values);
+    await this.props.putEvent(values);
     this.props.history.push("/");
   }
 
@@ -39,7 +44,7 @@ class EventsShow extends Component {
   }
 
   render() {
-    const { handleSubmit, pristine, submitting } = this.props;
+    const { handleSubmit, pristine, submitting, invalid } = this.props;
     return (
       <React.Fragment>
         <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -63,7 +68,7 @@ class EventsShow extends Component {
             <input
               type="submit"
               value="Submit"
-              disabled={pristine || submitting}
+              disabled={pristine || submitting || invalid}
             />
             <Link to="/">Cancel</Link>
             <Link to="/" onClick={this.onDeleteClick}>
@@ -85,9 +90,17 @@ const validate = (values) => {
   return errors;
 };
 
-const mapDispatchToProps = { deleteEvent };
+const mapStatetoProps = (state, ownProps) => {
+  const event = state.events[ownProps.match.params.id];
+  return { initialValues: event, event };
+};
+const mapDispatchToProps = { getEvent, deleteEvent, putEvent };
 
 export default connect(
-  null,
+  mapStatetoProps,
   mapDispatchToProps
-)(reduxForm({ validate, form: "eventShowForm" })(EventsShow));
+)(
+  reduxForm({ validate, form: "eventShowForm", enableReinitialize: true })(
+    EventsShow
+  )
+);
