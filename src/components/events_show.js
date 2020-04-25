d54cabaf99a4652619a4e629109526a0e4087e98
fuuukeee3 <semarusika@gmail.com>
2020-04-25 16:11:46 +0900

    material-ui show

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 import { Link } from "react-router-dom";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
 
 import { getEvent, deleteEvent, putEvent } from "../actions";
 
@@ -24,11 +26,16 @@ class EventsShow extends Component {
       type,
       meta: { touched, error },
     } = field;
+
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     );
   }
 
@@ -45,6 +52,8 @@ class EventsShow extends Component {
 
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props;
+    const style = { margin: 12 };
+
     return (
       <React.Fragment>
         <form onSubmit={handleSubmit(this.onSubmit)}>
@@ -64,17 +73,23 @@ class EventsShow extends Component {
               component={this.renderField}
             ></Field>
           </div>
-          <div>
-            <input
-              type="submit"
-              value="Submit"
-              disabled={pristine || submitting || invalid}
-            />
-            <Link to="/">Cancel</Link>
-            <Link to="/" onClick={this.onDeleteClick}>
-              Delete
-            </Link>
-          </div>
+
+          <RaisedButton
+            label="Submit"
+            type="submit"
+            style={style}
+            disabled={pristine || submitting || invalid}
+          />
+          <RaisedButton
+            label="Cancel"
+            style={style}
+            containerElement={<Link to="/" />}
+          />
+          <RaisedButton
+            label="Delete"
+            style={style}
+            onClick={this.onDeleteClick}
+          />
         </form>
       </React.Fragment>
     );
